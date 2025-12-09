f22ad22aea547976962f56047f6983f25c6db232
ssoooon1122-arch <ssoooon1122@gmail.com>
2025-12-09 16:03:15 +0900

    2222

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
+const header = document.querySelector('.header');
+const contentsSection = document.querySelector('.contents');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                header.classList.add('dark');
+            } else {
+                header.classList.remove('dark');
+            }
+        });
+    },
+    { threshold: 0.3 }
+);
+
+observer.observe(contentsSection);
+
+
+
